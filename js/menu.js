85d1677d3d09075cd57a2a7c5fcaaddcfc600f72
YtecSolutions <ytec@outlook.com.br>
2023-10-19 16:33:01 -0300

    botao menu

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+"use strict"
+// Selecionando o elemento (através de descendência)que acionará o menu
+const botaoMenu = document.querySelector("nav h2");
+
+//Selecionando a lista/menu
+const menu = document.querySelector(".menu");
+console.log(menu)
+
+//Selecionando o link que está dentro do nav h2
+
+// nesta forma vai direto para a Nav h2 a. (não percorre todo o document)
+const textoBotao = botaoMenu.querySelector("a");
+console.log(textoBotao);
+
